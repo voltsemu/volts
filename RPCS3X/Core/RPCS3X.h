9eb7618e7aa32e0ba17023736e5d0bb9102b919d
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2019-04-19 16:51:39 -0400

    start implementing logging

--- RPCS3X/Core/RPCS3X.h
+++ RPCS3X/Core/RPCS3X.h
@@ -7,23 +7,14 @@ namespace RPCS3X
 {
     using Str = Cthulhu::String;
 
-    enum class Channel
-    {
-        Emu,
-        GPU,
-        CPU,
-        IO,
-        Audio,
-        Utils,
-    };
-
     struct Logger
     {
-        void Debug(Channel C, Str Message);
-        void Info(Channel C, Str Message);
-        void Warn(Channel C, Str Message);
-        void Error(Channel C, Str Message);
-        void Fatal(Channel C, Str Message);
+        static Array<String> Channels;
+        void Debug(Str Channel, Str Message);
+        void Info(Str Channel, Str Message);
+        void Warn(Str Channel, Str Message);
+        void Error(Str Channel, Str Message);
+        void Fatal(Str Channel, Str Message);
     };
 
     struct Emulator
