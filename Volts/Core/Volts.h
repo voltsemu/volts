5be69d6519438d34839ea4a9243af51f120ebc4d
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2019-04-28 17:25:41 -0400

    nearly got it all working on windows
    its a different bug every time

--- Volts/Core/Volts.h
+++ Volts/Core/Volts.h
@@ -1,3 +1,11 @@
 ﻿#pragma once
 
-extern "C" { int VoltsMain(); }
+// put in an extern block for compatibilitys sake
+extern "C" { 
+
+// cant include <Meta/Macros.h> because visual studio needs to include this
+#if defined(_WIN32) || defined(_WIN64)
+    __declspec(dllexport)
+#endif
+    int VoltsMain(); 
+}
