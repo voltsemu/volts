0b03bdeee6f03bb1c92dec975336eb62cd707560
Elliot Haisley <apachehaisley@gmail.com>
2019-04-29 23:45:52 -0400

    start mac frontend and add app loop readme

--- Volts/Core/Utilities/Logger/Logger.cpp
+++ Volts/Core/Utilities/Logger/Logger.cpp
@@ -5,29 +5,6 @@
 namespace Volts
 {
     Level Log::LogLevel = Level::Warning;
-    namespace
-    {
-        static const Str TPrefix = "trace: ";
-        static const Str IPrefix = "info: ";
-        static const Str DPrefix = "debug: ";
-        static const Str WPrefix = "warning: ";
-        static const Str EPrefix = "error: ";
-        static const Str FPrefix = "fatal: ";
-    }
-
-    const Str& Prefix(Level S)
-    {
-        switch(S)
-        {
-            default:
-            case Level::Trace: return TPrefix;
-            case Level::Info: return IPrefix;
-            case Level::Debug: return DPrefix;
-            case Level::Warning: return WPrefix;
-            case Level::Error: return EPrefix;
-            case Level::Fatal: return FPrefix;
-        }
-    }
 
     void Log::Write(const Str& Channel, Level S, const Str& Message)
     {
--- Volts/Platform/Apple/Main.mm
+++ Volts/Platform/Apple/Main.mm
@@ -1,11 +1,39 @@
 #import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
 #include "../Interface/EmulatorWindow.h"
 #include <Volts/Core/Volts.h>
 
 struct MEmuWindow : Volts::EmulatorWindow
 {
+    NSWindow* Handle;
+
+    MEmuWindow()
+    {
+        [NSApplication sharedApplication];
+
+        Handle = [
+            [NSWindow alloc]
+            initWithContentRect:NSMakeRect(200, 200, 200, 200)
+                styleMask:NSWindowStyleMaskResizable | NSWindowStyleMaskClosable | NSWindowStyleMaskTitled
+                backing:NSBackingStoreBuffered
+                defer:NO
+        ];
+
+        [Handle setTitle:@"Volts"];
+
+        auto* View = [NSView new];
+        [Handle setContentView:View];
+        [Handle setAcceptsMouseMovedEvents:YES];
+
+        [Handle orderFrontRegardless];
+        [Handle setRestorable:NO];
+
+        [NSApp run];
+    }
+
     virtual ~MEmuWindow() override {}
+    
     virtual void WriteLog(const char* Channel, Volts::Level Severity, const char* Message) override
     {
         NSLog(@"%d %s: %s", Severity, Channel, Message);
