66b992eaf58ffb6ce273a6ed1937c4b686f07675
Elliot Haisley <apachehaisley@gmail.com>
2019-05-16 10:57:11 -0400

    start working on apple frontend

---
+++ Volts/Platform/Darwin/Volts/Volts/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Volts
+//
+//  Created by Student on 2019-05-16.
+//  Copyright © 2019 ApacheHB. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Volts/Platform/Darwin/Volts/Volts/GameView.h
@@ -0,0 +1,18 @@
+//
+//  GameView.h
+//  Volts
+//
+//  Created by Student on 2019-05-16.
+//  Copyright © 2019 ApacheHB. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GameView : NSCell
+@property NSImageView* Icon;
+
+@end
+
+NS_ASSUME_NONNULL_END
