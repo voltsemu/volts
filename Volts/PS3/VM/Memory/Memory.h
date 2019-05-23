4d42cfceb35b507b41c4f5c9459eb0e0431520f4
Elliot Haisley <apachehaisley@gmail.com>
2019-05-23 16:39:39 -0400

    start setting up cpu interpreter

---
+++ Volts/PS3/VM/Memory/Memory.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace Volts::PS3
+{
+    
+}
--- Volts/PS3/VM/VM.h
+++ Volts/PS3/VM/VM.h
@@ -1,56 +1,26 @@
 #pragma once
 
 #include "Meta/Aliases.h"
+#include "Meta/Macros.h"
 
-#include <intrin.h>
+#if OS_WINDOWS
+#   include <intrin.h>
+#else
+#   include <xmmintrin.h>
+#endif
 
 namespace Volts::PS3
 {
+    // alias this until i write a concrete class for it
     using Vec128 = __m128;
 
-    // the current state of the ppu thread
-    // this is different from the general ppu state
-    // as this represents emulator state rather than console state
-    enum class PPUThreadState : Cthulhu::U8
+    // the current state of a thread
+    enum class ThreadState : Cthulhu::U8
     {
-        None, // thread hasnt been started yet
-        Paused, // thread has been paused by emulator
-        Locked, // thread has been locked while state is being transfered
-        Running, // thread is running normally
-    };  
-
-    // all variables that represent a PPU threads state
-    struct PPUState
-    {
-        Cthulhu::U32 GPR[32]; // general purpose registers
-        Cthulhu::F32 FPR[32]; // floating point registers
-        Vec128 VR[32]; // vector registers
-
-        bool CR[32]; // condition registers
-    };
-
-    struct PPU : public PPUState
-    {
-        
-    };
-
-    struct SPU
-    {
-        Vec128 VR[128];
-    };
-
-    struct RSX
-    {
-
-    };
-
-    struct State
-    {
-
-    };
-
-    struct VM
-    {
-        Cthulhu::Byte* Memory;
+        None = (1 << 1), ///< the thread hasnt started yet or has shut down cleanly
+        Paused = (1 << 2), ///< the thread has been paused by the emulator for an indefinete amount of time
+        Frozen = (1 << 3), ///< the thread has been frozen for a short period so save states or other data transfers can occur
+        Running = (1 << 4), ///< the thread is running normally
+        Dead = (1 << 5), ///< the thread has crashed or has been irreversably broken in some way
     };
 }
