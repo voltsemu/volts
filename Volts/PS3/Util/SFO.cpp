702b83944d01c7d5c1c5330c7eee6fb46bfa6a70
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2019-06-02 15:28:02 -0400

    fix gcc compiler error

--- Volts/PS3/Util/SFO.cpp
+++ Volts/PS3/Util/SFO.cpp
@@ -6,19 +6,19 @@
 namespace Volts::PS3::SFO
 {
     using namespace Cthulhu;
-    
+
     // unlike the rest of the ps3s data structures this data is stored in little endian
-    
+
     // this serves as a redirector to a (key, value) pair stored in the file
     // we put it here instead of in the header as it is a purley internal data structure
-    // not used anywhere outside this function 
+    // not used anywhere outside this function
     struct IndexTableEntry
     {
         // relative key offset from the asbolute key table offset
         Little<U16> KeyOffset;
 
         // the format of the data type
-        Little<Format> Format;
+        Little<Format> DataFormat;
 
         // the actual length of the data
         Little<U32> DataLength;
@@ -54,7 +54,7 @@ namespace Volts::PS3::SFO
         //make sure the file is valid so we dont segfault
         if(!File.Valid())
             return None<SFO>();
-        
+
         // read the first sizeof(Header) bytes into a Header
         // this contains some basic metadata about the file structure
         // as well as magic numbers and version numbers
@@ -71,7 +71,7 @@ namespace Volts::PS3::SFO
         // create the object
         SFO Object;
 
-        // for every entry in the SFO 
+        // for every entry in the SFO
         for(U32 I = 0; I < Stats.TotalEntries; I++)
         {
             // read in the first redirector
@@ -85,7 +85,7 @@ namespace Volts::PS3::SFO
 
             // create a string to store the key
             String Key;
-            
+
             // read in the key on char at a time
             while(C8 C = File.Next())
                 Key += C;
@@ -93,13 +93,13 @@ namespace Volts::PS3::SFO
             // then seek to the absolute offset of the data table plus the relative data offset from the redirector
             File.Seek(Stats.DataOffset + Redirector.DataOffset);
 
-            // read in as many bytes as needed, 
+            // read in as many bytes as needed,
             // we go with MaxLength instead of DataLength
             // so that we get null bytes in strings
             Array<U8> Bytes = File.ReadBytes(Redirector.MaxLength);
 
             // create a value from the data we collected
-            Value Set = { Redirector.Format, Bytes };
+            Value Set = { Redirector.DataFormat, Bytes };
 
             // add it to the table
             Object[Key] = Set;
