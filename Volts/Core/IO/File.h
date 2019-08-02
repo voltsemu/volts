90a77d75789d3c7452099646b588da10ba69034e
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2019-08-01 22:42:09 -0400

    add some codew to implement later

--- Volts/Core/IO/File.h
+++ Volts/Core/IO/File.h
@@ -8,16 +8,32 @@
 #   include <stdio.h>
 #endif
 
+#if 0
+    only this works on windows, idk why. something about posix
+	HANDLE File = CreateFileA(
+		"volts_output.bin",
+		GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_READ | FILE_SHARE_WRITE,
+		nullptr,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		nullptr
+	);
+
+	FileSystem::BufferedFile F{"C:\\Users\\Elliot\\source\\repos\\RPCS3X\\Build\\EBOOT.BIN"};
+	auto S = UNSELF::DecryptSELF(F).Get();
+	WriteFile(
+		File,
+		S.GetData(),
+		S.Len(),
+		nullptr,
+		nullptr
+	);
+
+	FindClose(File);
+
+#endif
+
 namespace Volts
 {
-    struct File
-    {
-#if OS_WINDOWS
-        HANDLE Handle;
-#else
-        FILE* Handle;
-#endif
-    };
-
-    File Open();
 }
