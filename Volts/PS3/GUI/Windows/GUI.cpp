8c52f566e06b61c4653baabe0a634e063b7d985e
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2019-09-07 23:56:56 -0400

    everything is failling apart

--- Volts/PS3/GUI/Windows/GUI.cpp
+++ Volts/PS3/GUI/Windows/GUI.cpp
@@ -1,24 +1,11 @@
 #include "GUI.h"
 
-#include <winrt/Windows.ApplicationModel.Core.h>
-#include <winrt/Windows.UI.Core.h>
-
-using namespace winrt;
-using namespace winrt::Windows::ApplicationModel::Core;
-using namespace winrt::Windows::UI::Core;
+#include <winrt/Windows.Foundation.h>
 
 namespace Volts::PS3
 {
-    struct App : IFrameworkViewSource
-    {
-        IFrameworkView CreateView()
-        {
-            return IFrameworkView();
-        }
-    };
-
     GUI::GUI()
     {
-        CoreApplication::Run(App());
+        winrt::init_apartment();
     }
 }
