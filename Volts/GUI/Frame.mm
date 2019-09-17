a6b47985608ef5ae51c3b1a537a4ce1644e18ebf
Elliot Brown <apachehaisley@gmail.com>
2019-09-17 00:02:49 -0400

    start working on mac backend

--- Volts/GUI/Frame.mm
+++ Volts/GUI/Frame.mm
@@ -1,14 +1,61 @@
 #include "Frame.h"
 
+#import <Cococa/Cocoa.h>
+#import <MetalKit/MetalKit.h>
+
+@interface VApp : NSApplication
+@end
+@implementation VApp
+- (void)sendEvent:(NSEvent*)event
+{
+    if([event type] == NSEventTypeKeyUp && ([event modifierFlags] & NSEventModiferFlagCommand))
+        [[self keyWindow] sendEvent:event];
+    else
+        [super selfEvent:event];
+}
+@end
+
+@interface VWindowDelegate : NSObject<NSWindowDelegate>
+@end
+@implementation VWindowDelegate
+@end
+
+@interface VViewDelegate : NSObject<NSViewDelegate>
+@end
+@implementation VViewDelegate
+@end
+
+@interface VMTKView : MTKView
+@end
+@implementation VMTKView
+@end
+
+@interface VAppDelegate : NSObject<NSApplicationDelegate>
+@end
+@implementation VAppDelegate
+- (void)applicationDidFinishLaunching:(NSNotification*)notify
+{
+
+}
+@end
+
 namespace Volts::GUI
 {
-    Frame::Frame(
-        U32 Width,
-        U32 Height,
-        U32 X,
-        U32 Y
-    )
+    Frame::Frame()
     {
+        Handle = [
+            [NSWindow alloc]
+        ]
+    }
 
+    Frame& Frame::Title(const String& T) { this->T = T; return *this; }
+
+    void Frame::Run(Lambda<void()> Generator)
+    {
+        [VApp sharedApplication];
+        [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+        [NSApp setDelegate:[[VAppDelegate alloc] init]];
+        [NSApp activateIgnoringOtherApps:YES];
+        [NSApp run];
     }
 }
