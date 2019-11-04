3e0b0a6a916c78dfa53b09d136c0e2fa54dc6416
Elliot Brown <apachehaisley@gmail.com>
2019-11-04 07:35:10 -0500

    add firmware parsing

---
+++ Volts/Utils/PUP.cpp
@@ -0,0 +1,61 @@
+#include "PUP.h"
+
+#include "Core/Convert.h"
+#include "Core/Emulator.h"
+
+namespace Volts::Utils
+{
+    struct Header
+    {
+        U64 Magic;
+        Big<U64> PackageVersion;
+        Big<U64> ImageVersion;
+        Big<U64> FileCount;
+        Big<U64> HeaderLength;
+        Big<U64> DataLength;
+    };
+
+    std::optional<PUP::Object> LoadSFO(FS::BufferedFile& File)
+    {
+        PUP::Object Ret = { .File = File };
+
+        auto Head = File.Read<Header>();
+        if(Head.Magic != "SCEUF\0\0\0"_U64)
+        {
+            VERROR("PUP file had invalid magic");
+            return std::nullopt;
+        }
+
+        Ret.Files.reserve(Head.FileCount);
+        Ret.Hashes.reserve(Head.FileCount);
+
+        for(U32 I = 0; I < Head.FileCount; I++)
+            Ret.Files.push_back(File.Read<PUP::Entry>());
+
+        for(U32 I = 0; I < Head.FileCount; I++)
+            Ret.Hashes.push_back(File.Read<PUP::Hash>());
+
+        return Ret;
+    }
+
+    namespace PUP
+    {
+        Binary Object::GetFile(U64 ID)
+        {
+            for(auto& Entry : Files)
+            {
+                if(Entry.ID == ID)
+                {
+                    File.Seek(Entry.Offset);
+                    Binary Out;
+                    Out.Reserve(Entry.Length);
+                    File.ReadN(Out.GetData(), Entry.Length);
+
+                    return Out;
+                }
+            }
+
+            return {};
+        }
+    }
+}
---
+++ Volts/Utils/PUP.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <FileSystem/BufferedFile.h>
+#include <Core/Memory/Binary.h>
+
+#include "Core/Endian.h"
+#include "Core/Macros.h"
+
+#include <optional>
+#include <vector>
+
+namespace Volts::Utils
+{
+    using namespace Cthulhu;
+
+    namespace FS = Cthulhu::FileSystem;
+
+    namespace PUP
+    {
+        struct Entry
+        {
+            Big<U64> ID;
+            Big<U64> Offset;
+            Big<U64> Length;
+            Pad Padding[8];
+        };
+
+        struct Hash
+        {
+            Big<U64> ID;
+            Byte Hash[20];
+            Pad Padding[4];
+        };
+
+        struct Object
+        {
+            std::vector<Entry> Files;
+            std::vector<Hash> Hashes;
+
+            FS::BufferedFile& File;
+
+            Binary GetFile(U64 ID);
+        };
+    }
+
+    std::optional<PUP::Object> LoadPUP(FS::BufferedFile& File);
+}
