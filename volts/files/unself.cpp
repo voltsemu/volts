d51b0ad61156438883f8e6614035f1d75c3cbe35
Elliot Brown <apachehaisley@gmail.com>
2019-11-18 00:34:28 -0500

    unself work

--- volts/files/unself.cpp
+++ volts/files/unself.cpp
@@ -31,10 +31,19 @@ namespace volts::files::unself
     {
         struct header
         {
-
+            big<u64> type;
+            big<u64> app_info_offset;
+            big<u64> elf_offset;
+            big<u64> prog_header_offset;
+            big<u64> section_header_offset;
+            big<u64> section_info_offset;
+            big<u64> version_offset;
+            big<u64> control_offset;
+            big<u64> control_length;
+            pad padding[8];  
         };
 
-        //static_assert(sizeof(header) == 80);
+        static_assert(sizeof(header) == 80);
 
         struct control_info
         {
@@ -47,8 +56,42 @@ namespace volts::files::unself
                 struct 
                 {
                     big<u32> flags;
+                    pad padding[28];
+                } control_flags;
 
-                };
+                static_assert(sizeof(control_flags) == 32);
+
+                struct 
+                {
+                    u8 constant[20];
+                    u8 elf_digest[20];
+                    big<u64> required_version;
+                } elf_digest_40;
+
+                static_assert(sizeof(elf_digest_40) == 48);
+
+                struct
+                {
+                    u8 const_or_digest[20];
+                    pad padding[12];
+                } elf_digest_30;
+
+                static_assert(sizeof(elf_digest_30) == 32);
+
+                struct 
+                {
+                    u32 magic;
+                    big<u32> version;
+                    big<u32> drm_type;
+                    big<u32> app_type;
+                    byte content_id[48];
+                    byte digest[16];
+                    byte inv_digest[16];
+                    byte xor_digest[16];
+                    pad padding[16];
+                } npdrm_info;
+
+                static_assert(sizeof(npdrm_info) == 128);
             };
         };
     }
@@ -77,6 +120,8 @@ namespace volts::files::unself
         std::stringstream elf()
         {
             std::stringstream out;
+
+            return out;
         }
 
     private:
