3657d23777294ef9a5642aa38c859688e4da1850
Elliot Brown <apachehaisley@gmail.com>
2020-03-03 10:48:24 -0500

    add more glm stuff

--- volts/rsx/vulkan/support.h
+++ volts/rsx/vulkan/support.h
@@ -15,7 +15,10 @@
 
 #include <vulkan/vulkan.h>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include "volts.h"
 
