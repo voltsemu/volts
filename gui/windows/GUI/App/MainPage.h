881e8bc5a3df4ed00738faf80e2461bd553f15c2
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-03-20 23:40:42 -0400

    try to get xaml islands working

---
+++ gui/windows/GUI/App/MainPage.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::App::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::App::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
