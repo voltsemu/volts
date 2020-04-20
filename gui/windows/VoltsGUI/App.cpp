26ecb069985eff35d32f170b55351b7a6b9fa735
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-04-20 02:00:10 -0400

    add working windows gui with xaml islands
    took long enough. vs 19.6 is broken so it'll be difficult to add pages for now

---
+++ gui/windows/VoltsGUI/App.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "App.h"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::VoltsGUI::implementation
+{
+    App::App()
+    {
+        Initialize();
+
+        AddRef();
+        m_inner.as<::IUnknown>()->Release();
+    }
+
+    App::~App()
+    {
+        Close();
+    }
+}
