27c539e02d21d2b8c478ee7c5046bdb3a649ea7f
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-05-28 02:54:52 -0400

    pain

---
+++ src/cli.cpp
@@ -0,0 +1,7 @@
+#include <cmd/cmd.h>
+#include <fmt/core.h>
+
+int main(int argc, const char** argv)
+{
+    cmd::parse(argc, argv);
+}
--- subprojects/cmd/cmd/cmd.h
+++ subprojects/cmd/cmd/cmd.h
@@ -1,39 +1,5 @@
 #pragma once
 
-#include <string>
-#include <unordered_map>
-#include <functional>
-
 namespace cmd
 {
-    using callback = std::function<void(const std::string&)>;
-
-    struct arg
-    {
-        std::string_view desc;
-        callback func;
-    };
-
-    struct options
-    {
-        options(const std::string_view& n, const std::string_view& d)
-            : name(n)
-            , desc(d)
-        { }
-
-        void add(const std::string_view& name, const std::string_view& desc, callback func)
-        {
-            callbacks[name] = { desc, func };
-        }
-
-        std::string help()
-        {
-            
-        }
-
-    private:
-        const std::string_view name;
-        const std::string_view desc;
-        std::unordered_map<std::string_view, arg> callbacks;
-    };
 }
