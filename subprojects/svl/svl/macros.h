6b37c605ada42646b7348dc8c2c3c110f548da9c
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-06-02 23:01:34 -0400

    tyuryturyut

--- subprojects/svl/svl/macros.h
+++ subprojects/svl/svl/macros.h
@@ -2,11 +2,13 @@
 
 #include "svl/platform.h"
 
-#define STR_INNER(X)
-#define STR(X)
+#define STR_INNER(X) #X
+#define STR(X) STR_INNER(X)
+
+#define HERE (STR(__FILE__) ":" STR(__LINE__))
 
 #if SYS_WINDOWS
-#   define CHECK_ERRNO(expr) if (int err = (expr); err != -1) { svl::log::fatal(); }
+#   define CHECK_ERRNO(expr) if (int err = (expr); err != -1) { svl::log::fatal(HERE); }
 #else
-#   define CHECK_RESULT(expr) if (auto err = (expr); !err) { svl::log::fatal(); }
+#   define CHECK_RESULT(expr) if (auto err = (expr); !err) { svl::log::fatal(HERE); }
 #endif
