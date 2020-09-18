bbe8c80f8317766766b17a3c0521bde81d72e1e2
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-09-18 01:07:16 -0400

    need to either generate of hardcode round tables

--- subprojects/svl/svl/aes.cpp
+++ subprojects/svl/svl/aes.cpp
@@ -2,6 +2,18 @@
 
 #include "macros.h"
 
+#define GET_U32LE(A, B, C) \
+    {(A) = ((u32)(B)[(C)]) \
+        | ((u32)(B)[(C) + 1] << 8) \
+        | ((u32)(B)[(C) + 2] << 16) \
+        | ((u32)(B)[(C) + 3] << 24)}   
+
+#define SET_U32LE(A, B, C) \
+    {(B)[(C)] = (u8)(A); \
+    (B)[(C) + 1] = (u8)((A) >> 8); \
+    (B)[(C) + 2] = (u8)((A) >> 16); \
+    (B)[(C) + 3] = (u8)((A) >> 24);}
+
 namespace svl {
     int rounds(int size) {
         switch (size) {
@@ -11,8 +23,26 @@ namespace svl {
         default: ENSURE(false);
         }
     }
+
     void aes::setkey_enc(const u8* key, int size) {
         nr = rounds(size);
+
+        auto* keys = rk;
+
+        for (int i = 0; i < (size >> 5); i++) {
+            GET_U32LE(keys[i], key, i << 2);
+        }
+
+        switch (nr) {
+        case 10:
+            break;
+        case 12:
+            break;
+        case 14:
+            break;
+        default: 
+            ENSURE(false);
+        }
     }
 
     void aes::setkey_dec(const u8* key, int size) {
